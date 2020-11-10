8c092cb6e6490d9445601a03d4c0425cad95dfab
Elena <elgarcan@inf.upv.es>
2020-11-10 13:16:56 +0100

    added passport middleware

--- config/initializers/database.js
+++ config/initializers/database.js
@@ -1,12 +1,13 @@
 const Sequelize = require('sequelize');
 require('sequelize-hierarchy')(Sequelize);
+const dotenv = require('dotenv').config();
 
 const UserModel = require('../../src/models/user');
 const DbConfig = require("../config.json");
 
 
-const instance = new Sequelize(DbConfig.development.database, DbConfig.development.username, DbConfig.development.password, {
-    host: DbConfig.development.host,
+const instance = new Sequelize(dotenv.parsed.RDS_DBNAME, dotenv.parsed.RDS_USERNAME, dotenv.parsed.RDS_PASSWORD, {
+    host: dotenv.parsed.RDS_HOSTNAME,
     dialect: 'mysql',
     operatorsAliases: 0,
     logging: console.log,
--- config/initializers/server.js
+++ config/initializers/server.js
@@ -1,8 +1,71 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require('dotenv').config();
 const json = require('body-parser');
+const cors = require('cors');
+const Passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const UserService = require("../../src/services/common");
 
 const Server = express();
+
+/*
+    Server Middlewares
+*/
 Server.use(json.json());
+
+// enables cors
+Server.use(
+    cors({
+      allowedHeaders: ['sessionId', 'Content-Type', 'Authorization', 'method', 'internxt-version', 'internxt-client', 'internxt-mnemonic'],
+      exposedHeaders: ['sessionId'],
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+    }),
+);
+
+/**
+   * JWT configuration.
+   * Defines user authorization source (header / bearer token),
+   * and the password to verify the JWT
+   */
+  const passportOpts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: dotenv.parsed.JWT_SECRET,
+};
+
+/**
+* Passport strategy configuration.
+* Once JWT is granted, this middleware resolves the user info
+*/
+Passport.use(
+    new JwtStrategy(passportOpts, (payload, done) => {
+    /* Temporal compatibility with old JWT
+    * BEGIN
+    */
+    const COMPATIBILITY = true;
+    let email = payload;
+    if (typeof payload === 'object') {
+        email = payload.email;
+    } else if (!COMPATIBILITY) {
+        return done(new Error('Old JWT not supported'));
+    }
+    /* END
+    * After JWT migration, the email will be payload.email
+    * and delete this block + uncomment next line
+    */
+
+    // const email = payload.email
+
+    UserService.FindUserObjByEmail(email).then((user) => 
+        done(null, user)).catch((err) => {
+          console.log('Unauthorized %s', email)
+          done(err)
+        });
+    }),
+);
+
 
 
 Server.listen(8000);
